1084493f6e878acb4a48c7a6cc31892adc9dd21b
Mattia Zerbini <zermatty6@gmail.com>
2026-01-20 15:33:43 +0100

    Add class DAO, save

---
+++ U4-W3-D2/src/main/java/Mattiazerbini/dao/EventoDAO.java
@@ -0,0 +1,40 @@
+package Mattiazerbini.dao;
+
+import Mattiazerbini.entities.Evento;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.EntityTransaction;
+
+public class EventoDAO {
+    private final EntityManager entityManager;
+
+    public EventoDAO(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    //SAVE
+    public void save(Evento newEvento){
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(newEvento);
+
+        transaction.commit();
+
+        System.out.println("L'evento " +newEvento.getTitolo()+ "+ stato salvatonel database");
+    }
+
+
+    //GETBYID
+
+    public Evento findById(long idEvento) {
+        Evento found = entityManager.find(Evento.class, idEvento);
+        if (found == null) throw new EntityNotFoundException(idEvento);
+        return found;
+    }
+
+    public void getById(long idEvento){
+        
+    }
+}
